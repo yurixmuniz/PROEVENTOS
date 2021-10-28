6381ded260d52a5aa66c8b02c8a112b738095277
Yuri Muniz Maldonado <yuri.maldonado@castgroup.com.br>
2021-10-28 12:09:21 -0300

    initial

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(ILogger<EventoController> logger)
+        {
+
+        }
+
+        [HttpGet]
+        public List<Evento> Get()
+        {
+            return new List<Evento>()
+            {
+                new Evento()
+                {
+                    EventoId = 1,
+                    Local = "Chacara",
+                    DataEvento = "11/12/2021",
+                    Tema = "Churrasco",
+                    QtdPessoas = 10,
+                    Lote = "A1",
+                    ImageURL = "../teste.img"
+                },
+
+                 new Evento()
+                {
+                    EventoId = 2,
+                    Local = "Chacara2",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    Tema = "Natal",
+                    QtdPessoas = 20,
+                    Lote = "A3",
+                    ImageURL = "../teste.img"
+                }
+            };
+          
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Método Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Agora Put id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Agora Delete id = {id}";
+        }
+    }
+}
